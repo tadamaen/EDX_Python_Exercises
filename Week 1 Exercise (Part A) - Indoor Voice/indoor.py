c15979b1c9aa4180e38f09051ae3393b41e75bbd
Damaen Tan <tada.maenth@gmail.com>
2024-12-24 23:26:36 +0800

    Add files via upload

---
+++ Week 1 Exercise (Part A) - Indoor Voice/indoor.py
@@ -0,0 +1,9 @@
+# In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input
+# in lowercase. Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user
+# explicitly, as by passing a str of your own as an argument to input.
+
+# Step 1: Ask the user for his/her input
+name = input("What do you want to write? ")
+
+# Step 2: Change the name to lower case and print the name out
+print(name.lower())
